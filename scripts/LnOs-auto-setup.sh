9e9b478c96d40c9cc0ad96e76ff43122a629b4f1
ricey <truonghoangtampro@gmail.com>
2025-07-19 01:01:47 -0500

    refactor: rewrite auto-setup.sh using gum for

--- scripts/LnOs-auto-setup.sh
+++ scripts/LnOs-auto-setup.sh
@@ -1,39 +1,41 @@
+gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Hello, there. Welcome to LNOS auto setup script"
 
+#------------------------------------------------- DE INSTALLATION -------------------------------------------------#
 
+while true; do
+    DE_CHOICE=$(gum choose --header "Choose your Desktop Environment(DE):" "Gnome" "Hyprland" "KDE" "XFCE" "Skip DE Install")
 
+	if [[ "$DE_CHOICE" == "Skip DE Install" ]]; then
+		echo "Skipping DE installation."
+		break
+	fi
 
-DE_CHOICE="0"
-while [[ $DE_CHOICE == "0" ]]; do
-		echo "Choose a Desktop Environment:"
-		echo "1) Gnome"
-		echo "2) Hyprland"
-		echo "3) KDE"
-		echo "4) XFCE"
-		read -p "Enter your choice (1-4): " DE_CHOICE
-		if [[ $DE_CHOICE -lt 1 || $DE_CHOICE -gt 4 ]]; then
-				echo "Invalid Desktop Environment. Please choose again."
-				DE_CHOICE="0"
-		fi
-done	
+    gum confirm "You selected: $DE_CHOICE. Proceed with installation?" && break
 
-case DE_CHOICE in
-	1 )
-		echo "Choosen DE Gnome"
-		pacman -S --noconfirm xorg xorg-server gnome gdm 
-		systemctl enable --now gdm.service
-		;;
-	2 )
-		echo "Choosen DE Hyprland"
-		pacman -S --noconfirm wayland uwsm hyprland
-		;;
-	3 )
-		echo "Choosen DE KDE"
-		;;
-	4 )
-		echo "Choosen DE XFCE"
-		;;
-	* )
-		echo "ERROR unknown choice"
-		;;
-	esac
+    echo "Returning to selection menu..."
+done
+
+case "$DE_CHOICE" in
+    "Gnome")
+        echo "Chosen DE: Gnome"
+        # pacman -S --noconfirm xorg xorg-server gnome gdm
+        # systemctl enable --now gdm.service
+        ;;
+    "Hyprland")
+        echo "Chosen DE: Hyprland"
+        # pacman -S --noconfirm wayland uwsm hyprland
+        ;;
+    "KDE")
+        echo "Chosen DE: KDE"
+        # pacman -S --noconfirm xorg xorg-server plasma kde-desktop sddm
+		# systemctl enable --now sddm.service
+        ;;
+    "XFCE")
+        echo "Chosen DE: XFCE"
+        # pacman -S --noconfirm xorg xorg-server xfce4 xfce4-goodies lightdm lightdm-gtk-greeter
+        # systemctl enable --now lightdm.service
+        ;;
+esac
+
+#-----------------------------------------------------------------------------------------------------------------#
 
