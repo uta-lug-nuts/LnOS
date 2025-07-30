069cbdf49436fc0b3cf367246f183a5a46757e5e
betim-hodza <bhodza02@gmail.com>
2025-07-30 07:36:28 -0500

    update: added new tux logo and changed licensing to gpl3

--- scripts/Environment-setup.sh
+++ scripts/Environment-setup.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# LnOS
+# Copyright (C) 2025  UTA-LugNuts
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # goals of this file:
 # * allow users to choose a DE
 # * allow users to select the type of student they are (CSE, SWE, or Custom)
