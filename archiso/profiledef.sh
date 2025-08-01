d156224d51f8e1ab43d2e7985469a81069c85fd6
Trevor Bakker <trevor.bakker@protonmail.com>
2025-08-01 12:38:59 +0000

    A few updates for Parallels

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -3,17 +3,18 @@
 iso_name="lnos"
 iso_label="LNOS_$(date +%Y%m)"
 iso_publisher="UTA-LugNuts <https://github.com/uta-lug-nuts/LnOS>"
-iso_application="LnOS Live/Rescue CD"
+iso_application="LnOS Install CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
+esp_size_mb="128"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
            'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
            'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '22' '-b' '1M')
 file_permissions=(
   ["/root"]="0:0:750"
   ["/usr/local/bin/LnOS-installer.sh"]="0:0:755"
